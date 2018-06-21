986e966ca31b698cec9be419eddd13591a73faa1
kronchan <k@wuwii.com>
2018-06-21 17:10:47 +0800

    update

--- auth-server/src/main/java/com/hfcsbc/security/DomainUserDetailsService.java
+++ auth-server/src/main/java/com/hfcsbc/security/DomainUserDetailsService.java
@@ -1,22 +1,14 @@
 package com.hfcsbc.security;
 
-import com.hfcsbc.domain.SysUser;
 import com.hfcsbc.repository.SysUserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by wangyunfei on 2017/6/9.
@@ -27,10 +19,12 @@ public class DomainUserDetailsService implements UserDetailsService {
 
     @Autowired
     private  SysUserRepository sysUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String lowcaseUsername = username.toLowerCase();
+       /* String lowcaseUsername = username.toLowerCase();
         Optional<SysUser> realUser = sysUserRepository.findOneWithRolesByUsername(lowcaseUsername);
 
 
@@ -39,5 +33,9 @@ public class DomainUserDetailsService implements UserDetailsService {
             Set<GrantedAuthority> grantedAuthorities = user.getAuthorities();
             return new User(user.getUsername(),user.getPassword(),grantedAuthorities);
         }).orElseThrow(() -> new UsernameNotFoundException("用户" + lowcaseUsername + "不存在!"));
+    */
+        return new User("kronchan", passwordEncoder.encode("123456"), AuthorityUtils
+                .commaSeparatedStringToAuthorityList("query-demo"));
     }
+
 }
